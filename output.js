b7f87de3565c7b8f35dac53eecabd2d8b9452fce
Joseph Lin <josephlin55555@gmail.com>
2015-04-27 18:23:02 -0700

    Removed Fields

--- output.js
+++ output.js
@@ -2,7 +2,6 @@ $('.submit').on('click', function() { //submit handles click and enter
   //grab message and user
   var message = $('.inputMessage').val();
   var user = $('.inputUser').val();
-  var points = '<span class="points">0</span>'
   
   //clear input box
   $('.inputMessage').val('');
@@ -13,14 +12,16 @@ $('.submit').on('click', function() { //submit handles click and enter
 
   var buttons = '<button class="like">Like</button> <button class="dislike">Dislike</button>';
 
-  $('.output').prepend('<div class="container"> <span class="message">' + user + ' says '  + message + '</span>' + buttons + points + '</div>');
-});
+  $('.output').prepend('<div class="container"><span class="message">' + user + ' says '  + message + '</span>' + buttons + '</div> </span>');
 
-$('.like').on('click', function() {
+  $('.like').on('click', function() {
 
-});
+  });
 
-$('.dislike').on('click', function() {
+
+  $('.dislike').on('click', function() {
+
+  });
 
 });
 
